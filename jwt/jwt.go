9bc5a4953b8e0282c0c3aa0ed50d5b1d3ce60989
enahomurphy <enahomurphy@gmail.com>
2018-11-12 06:46:31 +0100

    Update encoder to use baseb4url encode

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -42,9 +42,9 @@ type Payload struct {
 // Base64Encode takes in a string and returns
 // a bases64 encode string
 func Base64Encode(src string) string {
-	data := []byte(src)
-	str := base64.StdEncoding.EncodeToString(data)
-	return str
+	return strings.
+		TrimRight(base64.URLEncoding.
+			EncodeToString([]byte(src)), "=")
 }
 
 // Base64Decode takes in an Encoded string and returns
@@ -52,11 +52,16 @@ func Base64Encode(src string) string {
 // decoding the string, an error is returned with an
 // empty string
 func Base64Decode(src string) (string, error) {
-	decoded, err := base64.StdEncoding.DecodeString(src)
+	if l := len(src) % 4; l > 0 {
+		src += strings.Repeat("=", 4-l)
+	}
+
+	decoded, err := base64.URLEncoding.DecodeString(src)
 	if err != nil {
 		errMsg := fmt.Errorf("Decoding Error %s", err)
 		return "", errMsg
 	}
+
 	return string(decoded), nil
 }
 
